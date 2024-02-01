6c019bae08170656448999b3b7fce96e005f0e78
phuchaitt3 <phuchaitt3@gmail.com>
2024-02-01 11:20:18 +0700

    [m] (Admin stat) Grid border & padding

--- app/src/main/java/com/zebrand/app1food30s/ui/admin_statistics/AdminStatisticsActivity.kt
+++ app/src/main/java/com/zebrand/app1food30s/ui/admin_statistics/AdminStatisticsActivity.kt
@@ -8,10 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.zebrand.app1food30s.R
 
 class AdminStatisticsActivity : AppCompatActivity() {
-
-    // Lateinit properties for RecyclerViews, will be initialized in onCreate
     private lateinit var myGridRecyclerView: RecyclerView
-    private lateinit var anotherRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,21 +31,15 @@ class AdminStatisticsActivity : AppCompatActivity() {
         // Setting the adapter to the RecyclerView
         myGridRecyclerView.adapter = myGridAdapter
         // Define the layout as a GridLayoutManager with 2 columns
-        myGridRecyclerView.layoutManager = GridLayoutManager(this, 2)
+        val spanCount = 2 // Number of columns
+        myGridRecyclerView.layoutManager = GridLayoutManager(this, spanCount)
+
+        // Add item decoration for spacing
+        val spacing = 60 // Spacing in pixels
+        val includeEdge = true
+        myGridRecyclerView.addItemDecoration(GridSpacingItemDecoration(spanCount, spacing, includeEdge))
+
         // Improve performance by setting to a fixed size, as changes in content do not change the layout size
         myGridRecyclerView.setHasFixedSize(true)
-
-
-//        // Setup for another RecyclerView with a linear layout
-//        // Find the second RecyclerView widget as defined in the XML layout
-//        anotherRecyclerView = findViewById(R.id.another_recycler_view) // Ensure this ID matches the one in your XML layout
-//        // Create an adapter for this RecyclerView with a different set of data
-//        val anotherAdapter = MyAdapter(arrayOf("Item 1", "Item 2", "Item 3", "Item 4"))
-//        // Setting the adapter to the RecyclerView
-//        anotherRecyclerView.adapter = anotherAdapter
-//        // Define the layout as a LinearLayoutManager
-//        anotherRecyclerView.layoutManager = LinearLayoutManager(this)
-//        // Improve performance by setting to a fixed size, as changes in content do not change the layout size
-//        anotherRecyclerView.setHasFixedSize(true)
     }
 }
--- app/src/main/java/com/zebrand/app1food30s/ui/admin_statistics/MyGridAdapter.kt
+++ app/src/main/java/com/zebrand/app1food30s/ui/admin_statistics/MyGridAdapter.kt
@@ -1,5 +1,6 @@
 package com.zebrand.app1food30s.ui.admin_statistics
 
+import android.graphics.Rect
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -45,4 +46,28 @@ class MyGridAdapter(private val mDataset: Array<GridItem>) :
         val number: Int,
         @DrawableRes val imageResId: Int
     )
+}
+
+class GridSpacingItemDecoration(private val spanCount: Int, private val spacing: Int, private val includeEdge: Boolean) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        val position = parent.getChildAdapterPosition(view) // item position
+        val column = position % spanCount // item column
+
+        if (includeEdge) {
+            outRect.left = spacing - column * spacing / spanCount
+            outRect.right = (column + 1) * spacing / spanCount
+
+            if (position < spanCount) { // top edge
+                outRect.top = spacing
+            }
+            outRect.bottom = spacing // item bottom
+        } else {
+            outRect.left = column * spacing / spanCount
+            outRect.right = spacing - (column + 1) * spacing / spanCount
+            if (position >= spanCount) {
+                outRect.top = spacing // item top
+            }
+        }
+    }
 }
