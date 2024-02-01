902c85286b12718e2389951d52ae63977b6ef1eb
phuchaitt3 <phuchaitt3@gmail.com>
2024-02-01 14:13:00 +0700

    [+] (Admin stat) Firebase connected

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,9 +41,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-firestore:24.10.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation("com.google.firebase:firebase-firestore-ktx")
 }
--- app/src/main/java/com/zebrand/app1food30s/ui/admin_statistics/AdminStatisticsActivity.kt
+++ app/src/main/java/com/zebrand/app1food30s/ui/admin_statistics/AdminStatisticsActivity.kt
@@ -1,10 +1,12 @@
 package com.zebrand.app1food30s.ui.admin_statistics
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 import com.zebrand.app1food30s.R
 
 class AdminStatisticsActivity : AppCompatActivity() {
@@ -19,17 +21,20 @@ class AdminStatisticsActivity : AppCompatActivity() {
         // Find the RecyclerView widget as defined in the XML layout
         myGridRecyclerView = findViewById(R.id.my_grid_recycler_view)
         // Create an adapter for the grid RecyclerView with predefined data
-        // Modified to contain 6 elements for a 3x2 grid
-        val myGridAdapter = MyGridAdapter(
-            arrayOf(
-                MyGridAdapter.GridItem("Text 1", 1, R.drawable.image),
-                MyGridAdapter.GridItem("Text 2", 2, R.drawable.image),
-                MyGridAdapter.GridItem("Text 3", 3, R.drawable.image),
-                MyGridAdapter.GridItem("Text 4", 4, R.drawable.image),
+
+        // Fetch total number of orders
+        getTotalNumberOfOrders { totalOrders ->
+            val myGridAdapter = MyGridAdapter(
+                arrayOf(
+                    MyGridAdapter.GridItem("Text 1", totalOrders, R.drawable.image),
+                    MyGridAdapter.GridItem("Text 2", 2, R.drawable.image),
+                    MyGridAdapter.GridItem("Text 3", 3, R.drawable.image),
+                    MyGridAdapter.GridItem("Text 4", 4, R.drawable.image),
+                )
             )
-        )
-        // Setting the adapter to the RecyclerView
-        myGridRecyclerView.adapter = myGridAdapter
+            myGridRecyclerView.adapter = myGridAdapter
+        }
+
         // Define the layout as a GridLayoutManager with 2 columns
         val spanCount = 2 // Number of columns
         myGridRecyclerView.layoutManager = GridLayoutManager(this, spanCount)
@@ -42,4 +47,17 @@ class AdminStatisticsActivity : AppCompatActivity() {
         // Improve performance by setting to a fixed size, as changes in content do not change the layout size
         myGridRecyclerView.setHasFixedSize(true)
     }
+}
+
+private fun getTotalNumberOfOrders(callback: (Int) -> Unit) {
+    val db = FirebaseFirestore.getInstance()
+    db.collection("orders").get()
+        .addOnSuccessListener { documents ->
+            val totalOrders = documents.size()
+            callback(totalOrders)
+        }
+        .addOnFailureListener { exception ->
+            Log.w("Firestore", "Error getting documents: ", exception)
+            callback(0) // Handle error case as needed
+        }
 }
