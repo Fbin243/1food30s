2f7752f5f816807724f1c253823a5a199cc065dc
21127197 <ndtuan21@clc.fitus.edu.vn>
2024-02-13 17:19:08 +0700

    config-font-dims-color-&-finish-login-screen

--- app/src/main/java/com/zebrand/app1food30s/data/authentication/LoginActivity.kt
+++ app/src/main/java/com/zebrand/app1food30s/data/authentication/LoginActivity.kt
@@ -1,11 +1,14 @@
-package com.zebrand.app1food30s
+package com.zebrand.app1food30s.data.authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zebrand.app1food30s.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+
     }
 }
