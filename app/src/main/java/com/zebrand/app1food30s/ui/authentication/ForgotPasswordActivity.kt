be08fb7d93597687687463a46d1e2106504c1aaa
21127197 <ndtuan21@clc.fitus.edu.vn>
2024-02-14 15:54:02 +0700

    [finish] login-signUp-forgotPass

--- app/src/main/java/com/zebrand/app1food30s/ui/authentication/ForgotPasswordActivity.kt
+++ app/src/main/java/com/zebrand/app1food30s/ui/authentication/ForgotPasswordActivity.kt
@@ -1,12 +1,36 @@
 package com.zebrand.app1food30s.ui.authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.zebrand.app1food30s.R
 
 class ForgotPasswordActivity : AppCompatActivity() {
+    private lateinit var tv_login: TextView
+    private lateinit var back_icon: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+
+        init()
+        events()
+    }
+
+    private fun init(){
+        tv_login = findViewById(R.id.tv_login)
+        back_icon = findViewById(R.id.back_icon)
+    }
+
+    private fun events(){
+        tv_login.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        back_icon.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/zebrand/app1food30s/ui/authentication/LoginActivity.kt
+++ app/src/main/java/com/zebrand/app1food30s/ui/authentication/LoginActivity.kt
@@ -1,13 +1,20 @@
 package com.zebrand.app1food30s.ui.authentication
 
+import android.content.Intent
 import android.content.res.Configuration
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
 import com.zebrand.app1food30s.R
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var tv_sign_up:TextView
+    private lateinit var tv_forgot_password:TextView
+    private lateinit var back_icon:LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,6 +27,29 @@ class LoginActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_login)
 
+        init()
+        events()
+    }
 
+    private fun init(){
+        tv_sign_up = findViewById(R.id.tv_sign_up)
+        tv_forgot_password = findViewById(R.id.tv_forgot_password)
+        back_icon = findViewById(R.id.back_icon)
+    }
+
+    private fun events(){
+        tv_sign_up.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        tv_forgot_password.setOnClickListener {
+            val intent = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+//        back_icon.setOnClickListener {
+//            onBackPressedDispatcher.onBackPressed()
+//        }
     }
 }
