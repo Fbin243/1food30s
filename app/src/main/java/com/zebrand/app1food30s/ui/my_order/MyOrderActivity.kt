1986fdc7fb9b46e3f55fe3723b71b1bf99b96377
21127197 <ndtuan21@clc.fitus.edu.vn>
2024-02-14 22:28:51 +0700

    [finish] - my-order UI

---
+++ app/src/main/java/com/zebrand/app1food30s/ui/my_order/MyOrderActivity.kt
@@ -0,0 +1,22 @@
+package com.zebrand.app1food30s.ui.my_order
+
+import android.content.res.Configuration
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.zebrand.app1food30s.R
+
+class MyOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) {
+                // Dark mode is enabled, so force light mode
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+        setContentView(R.layout.activity_my_order)
+    }
+}
