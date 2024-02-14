f8966faf2c0eb4648579d9fc3d626d68006de749
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2024-02-14 17:32:51 +0700

    fix: comment conflicts on main
    comment conflicts of activity_forgot_password

--- app/src/main/java/com/zebrand/app1food30s/ui/authentication/LoginActivity.kt
+++ app/src/main/java/com/zebrand/app1food30s/ui/authentication/LoginActivity.kt
@@ -4,12 +4,9 @@ import android.content.res.Configuration
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
 import android.util.Log
 import android.widget.LinearLayout
 import android.widget.TextView
-=======
->>>>>>> parent of be08fb7 ([finish] login-signUp-forgotPass)
 import androidx.appcompat.app.AppCompatDelegate
 import com.zebrand.app1food30s.R
 
