ba9ba8367769a1b47c09149979760a49ac167f30
21127197 <ndtuan21@clc.fitus.edu.vn>
2024-02-14 15:13:11 +0700

    [fix] login UI in real device

---
+++ app/src/main/java/com/zebrand/app1food30s/ui/authentication/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.zebrand.app1food30s.ui.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zebrand.app1food30s.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
