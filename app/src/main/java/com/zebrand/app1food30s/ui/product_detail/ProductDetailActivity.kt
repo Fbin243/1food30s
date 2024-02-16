12217fadf899de60704963b5151fc7b9a7a611f1
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2024-02-16 22:04:05 +0700

    feat: page detail without reviews and ratings

---
+++ app/src/main/java/com/zebrand/app1food30s/ui/product_detail/ProductDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.zebrand.app1food30s.ui.product_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zebrand.app1food30s.R
+
+class ProductDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
