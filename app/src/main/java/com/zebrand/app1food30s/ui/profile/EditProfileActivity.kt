b3370211609cacb0904b88b7925cf9345b31bdca
dinhquytrieu <dqtrieu21@clc.fitus.edu.vn>
2024-03-04 15:56:18 +0700

    basically done FE activity edit profile (customer side)

---
+++ app/src/main/java/com/zebrand/app1food30s/ui/profile/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.zebrand.app1food30s.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zebrand.app1food30s.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
