6a420f9cc55bd035094bbd24aa2fa56b19105a84
phuchaitt3 <phuchaitt3@gmail.com>
2024-03-12 13:56:06 +0700

    Model changes: Product, Review

--- app/src/main/java/com/zebrand/app1food30s/data/Review.kt
+++ app/src/main/java/com/zebrand/app1food30s/data/Review.kt
@@ -1,3 +1,12 @@
 package com.zebrand.app1food30s.data
 
-class Review(var avatar: Int, var name: String, var rating: Int, var content: String, var date: String)
+import java.util.Date
+
+//class Review(var avatar: Int, var name: String, var rating: Int, var content: String, var date: String)
+
+data class Review(
+    val idAccount: String,
+    val rating: Double,
+    val content: String,
+    val date: Date
+)
