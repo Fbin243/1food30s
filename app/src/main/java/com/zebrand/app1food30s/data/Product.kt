215cfcc8ad4224fd51d1aa90ac25a559a967b5c2
phuchaitt3 <phuchaitt3@gmail.com>
2024-03-14 00:07:45 +0700

    Product model

--- app/src/main/java/com/zebrand/app1food30s/data/Product.kt
+++ app/src/main/java/com/zebrand/app1food30s/data/Product.kt
@@ -5,15 +5,15 @@ import java.util.Date
 //class Product(var img: Int, var title: String, var description: String, var price: Double)
 
 data class Product(
-    val id: String,
-    val idCategory: String,
-    val idOffer: String,
-    val name: String,
-    val image: String, // Assuming you're using resource IDs for sample images
-    val price: Double,
-    val description: String,
-    val stock: Int,
-    val sold: Int,
-    val reviews: List<Review>,
-    val date: Date // Date the product is created
+    val id: String = "",
+    val idCategory: String = "",
+    val idOffer: String = "",
+    val name: String = "",
+    val image: String = "", // Firebase Storage
+    val price: Double = 0.0,
+    val description: String = "",
+    val stock: Int = 0,
+    val sold: Int = 0,
+    val reviews: List<Review> = emptyList(),
+    val date: Date = Date() // Date the product is created
 )
