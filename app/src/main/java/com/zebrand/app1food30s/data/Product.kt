d28c3e14a0cf0f08e90d75356014e34b7594232f
dinhquytrieu <dqtrieu21@clc.fitus.edu.vn>
2024-03-13 14:36:43 +0700

    update FE edit profile

--- app/src/main/java/com/zebrand/app1food30s/data/Product.kt
+++ app/src/main/java/com/zebrand/app1food30s/data/Product.kt
@@ -2,18 +2,18 @@ package com.zebrand.app1food30s.data
 
 import java.util.Date
 
-//class Product(var img: Int, var title: String, var description: String, var price: Double)
+class Product(var img: Int, var title: String, var description: String, var price: Double)
 
-data class Product(
-    val id: String,
-    val idCategory: String,
-    val idOffer: String,
-    val name: String,
-    val image: String, // Assuming you're using resource IDs for sample images
-    val price: Double,
-    val description: String,
-    val stock: Int,
-    val sold: Int,
-    val reviews: List<Review>,
-    val date: Date // Date the product is created
-)
+//data class Product(
+//    val id: String,
+//    val idCategory: String,
+//    val idOffer: String,
+//    val name: String,
+//    val image: String, // Assuming you're using resource IDs for sample images
+//    val price: Double,
+//    val description: String,
+//    val stock: Int,
+//    val sold: Int,
+//    val reviews: List<Review>,
+//    val date: Date // Date the product is created
+//)
