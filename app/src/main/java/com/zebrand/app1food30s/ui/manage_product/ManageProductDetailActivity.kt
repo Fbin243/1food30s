916594952fc5fa0db0f6ba42aa991b25bcbd6a04
dinhquytrieu <dqtrieu21@clc.fitus.edu.vn>
2024-03-20 17:56:09 +0700

    finish BE upload product

--- app/src/main/java/com/zebrand/app1food30s/ui/manage_product/ManageProductDetailActivity.kt
+++ app/src/main/java/com/zebrand/app1food30s/ui/manage_product/ManageProductDetailActivity.kt
@@ -1,8 +1,11 @@
 package com.zebrand.app1food30s.ui.manage_product
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
@@ -11,10 +14,14 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.zebrand.app1food30s.data.Category
 import com.zebrand.app1food30s.data.Product
-import com.zebrand.app1food30s.R // Change to your actual package name
+import com.zebrand.app1food30s.R
 import java.util.Date
+import java.util.UUID
+import com.google.firebase.storage.ktx.storage
 
 class ManageProductDetailActivity : AppCompatActivity() {
+
+    private val PICK_IMAGE_REQUEST = 71 // Unique request code
 
     private lateinit var nameEditText: TextInputEditText
     private lateinit var priceEditText: TextInputEditText
@@ -24,10 +31,14 @@ class ManageProductDetailActivity : AppCompatActivity() {
     private lateinit var categorySpinner: Spinner
     private lateinit var offerSpinner: Spinner
 
+    private lateinit var productImageView: ImageView
+
+    // Lưu URI của hình ảnh tạm thời
+    private var imageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_product_detail)
-
 
         nameEditText = findViewById(R.id.input_name)
         priceEditText = findViewById(R.id.input_price)
@@ -36,81 +47,112 @@ class ManageProductDetailActivity : AppCompatActivity() {
         createButton = findViewById(R.id.create_btn)
         categorySpinner = findViewById(R.id.category_spinner)
         offerSpinner = findViewById(R.id.offer_spinner)
+        productImageView = findViewById(R.id.image_product)
         loadCategoriesFromFirebase()
         loadOffersFromFirebase()
 
         createButton.setOnClickListener {
             saveProductToFirestore()
         }
+
+        productImageView.setOnClickListener {
+            startImagePicker()
+        }
+    }
+
+    private fun startImagePicker() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
+            imageUri = data.data!!
+            productImageView.setImageURI(imageUri)
+        }
     }
 
     private fun saveProductToFirestore() {
+        val productName = nameEditText.text.toString().trim()
+        val db = Firebase.firestore
+
+        imageUri?.let { uri ->
+            val fileName = "product${UUID.randomUUID()}.png" // Tạo một tên file duy nhất
+            val storageReference = Firebase.storage.reference.child("images/product/$fileName")
+            val uploadTask = storageReference.putFile(uri)
+
+            uploadTask.addOnSuccessListener {
+                // Khi tải lên thành công, lưu đường dẫn cố định vào Firestore thay vì URL
+                val imagePath = "images/product/$fileName" // Đường dẫn này sẽ được lưu trong Firestore
+                createAndSaveProduct(imagePath)
+            }.addOnFailureListener {
+                // Xử lý lỗi
+            }
+        } ?: run {
+            // Lưu sản phẩm mà không có hình ảnh
+            createAndSaveProduct("")
+        }
+    }
+
+    private fun createAndSaveProduct(imagePath: String) {
         val productName = nameEditText.text.toString().trim()
         val productPrice = priceEditText.text.toString().toDoubleOrNull() ?: 0.0
         val productStock = stockEditText.text.toString().toIntOrNull() ?: 0
         val productDescription = descriptionEditText.text.toString().trim()
         val db = Firebase.firestore
 
-        // Get the selected names from the spinners
         val selectedCategoryName = categorySpinner.selectedItem.toString()
         val selectedOfferName = offerSpinner.selectedItem.toString()
 
-        // First, find the category reference
         db.collection("categories").whereEqualTo("name", selectedCategoryName).limit(1).get()
             .addOnSuccessListener { categoryDocuments ->
                 if (categoryDocuments.documents.isNotEmpty()) {
                     val categoryDocumentRef = categoryDocuments.documents.first().reference
 
-                    // Then, find the offer reference
                     db.collection("offers").whereEqualTo("name", selectedOfferName).limit(1).get()
                         .addOnSuccessListener { offerDocuments ->
                             if (offerDocuments.documents.isNotEmpty()) {
                                 val offerDocumentRef = offerDocuments.documents.first().reference
 
-                                // Create a new document reference with a unique ID in the "products" collection
                                 val newProductRef = db.collection("products").document()
 
-                                // Create the product with the new document's ID
                                 val newProduct = Product(
                                     id = newProductRef.id,
                                     name = productName,
                                     idCategory = categoryDocumentRef,
-                                    idOffer = offerDocumentRef,
+                                    idOffer =                                 offerDocumentRef,
                                     price = productPrice,
                                     description = productDescription,
                                     stock = productStock,
-                                    image = "", // Update this after image upload if necessary
-                                    date = Date() // Current date
+                                    image = imagePath, // Sử dụng URL của hình ảnh đã tải lên
+                                    date = Date()
                                 )
 
-                                // Set the new document with the new product
                                 newProductRef.set(newProduct)
                                     .addOnSuccessListener {
-                                        // Handle success, e.g., show a success message to the user
+                                        // Xử lý thành công, ví dụ: hiển thị thông báo thành công cho người dùng
                                     }
                                     .addOnFailureListener { e ->
-                                        // Handle failure, e.g., show an error message to the user
+                                        // Xử lý thất bại, ví dụ: hiển thị thông báo lỗi cho người dùng
                                     }
                             } else {
-                                // Handle the case where no matching offer is found
+                                // Xử lý trường hợp không tìm thấy ưu đãi phù hợp
                             }
                         }
                         .addOnFailureListener { exception ->
-                            // Handle any errors in finding the offer
+                            // Xử lý lỗi khi tìm kiếm ưu đãi
                         }
                 } else {
-                    // Handle the case where no matching category is found
+                    // Xử lý trường hợp không tìm thấy danh mục phù hợp
                 }
             }
             .addOnFailureListener { exception ->
-                // Handle any errors in finding the category
+                // Xử lý lỗi khi tìm kiếm danh mục
             }
     }
-
-
-    // Dummy functions, replace these with your actual logic for retrieving document IDs based on the spinner selections
-
-
 
     private fun loadCategoriesFromFirebase() {
         val db = Firebase.firestore
@@ -119,17 +161,14 @@ class ManageProductDetailActivity : AppCompatActivity() {
         db.collection("categories").get()
             .addOnSuccessListener { documents ->
                 for (document in documents) {
-//                    val category = document.toObject(Category::class.java)
                     categoriesList.add(document.getString("name") ?: "")
                 }
-                // Update the spinner
                 val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categoriesList)
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                 categorySpinner.adapter = adapter
             }
             .addOnFailureListener { exception ->
-                // Handle any errors here, for example:
-                // Log.d("ManageProductDetailActivity", "Error getting documents: ", exception)
+                // Xử lý lỗi ở đây
             }
     }
 
@@ -140,17 +179,15 @@ class ManageProductDetailActivity : AppCompatActivity() {
         db.collection("offers").get()
             .addOnSuccessListener { documents ->
                 for (document in documents) {
-//                    val category = document.toObject(Category::class.java)
                     offersList.add(document.getString("name") ?: "")
                 }
-                // Update the spinner
                 val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, offersList)
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                 offerSpinner.adapter = adapter
             }
             .addOnFailureListener { exception ->
-                // Handle any errors here, for example:
-                // Log.d("ManageProductDetailActivity", "Error getting documents: ", exception)
+                // Xử lý lỗi ở đây
             }
     }
 }
+
