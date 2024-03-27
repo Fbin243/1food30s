fff727f66fd94a3e003275b09cf8a7714e400352
dinhquytrieu <dqtrieu21@clc.fitus.edu.vn>
2024-03-27 09:48:13 +0700

    FE feature filter in manage product

--- app/src/main/java/com/zebrand/app1food30s/ui/manage_product/ManageProductActivity.kt
+++ app/src/main/java/com/zebrand/app1food30s/ui/manage_product/ManageProductActivity.kt
@@ -1,14 +1,19 @@
 package com.zebrand.app1food30s.ui.manage_product
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.zebrand.app1food30s.R
@@ -23,6 +28,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class ManageProductActivity : AppCompatActivity() {
     private lateinit var binding: ActivityManageProductBinding
@@ -30,6 +38,10 @@ class ManageProductActivity : AppCompatActivity() {
     private val fireStore = FirebaseFirestore.getInstance()
     private val fireStorage = FirebaseStorage.getInstance()
     private lateinit var addButton: ImageView
+    private lateinit var filterButton: ImageView
+    private lateinit var botDialog: BottomSheetDialog
+    lateinit var statusArr: Array<String>
+    val customerArr = arrayOf("1- Bedroom", "2- Bedroom", "3- Bedroom")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +51,15 @@ class ManageProductActivity : AppCompatActivity() {
         handleDisplayProductList()
 
         addButton = findViewById(R.id.add_product_btn)
+        filterButton = findViewById(R.id.filter_btn)
 
         addButton.setOnClickListener {
             val intent = Intent(this, ManageProductDetailActivity::class.java)
             startActivity(intent)
+        }
+
+        filterButton.setOnClickListener {
+            showBottomSheet()
         }
     }
 
@@ -57,6 +74,55 @@ class ManageProductActivity : AppCompatActivity() {
             binding.productRcv.layoutManager = LinearLayoutManager(this@ManageProductActivity)
             binding.productRcv.adapter = adapter
         }
+    }
+
+    private fun showBottomSheet() {
+        val dialogView = layoutInflater.inflate(R.layout.bottom_sheet_manage_order, null)
+        botDialog = BottomSheetDialog(this, R.style.BottomSheetDialogTheme)
+        botDialog.setContentView(dialogView)
+
+        // status dropdown
+        val adapterStatus = ArrayAdapter(this, R.layout.item_drop_down_filter, statusArr)
+        val tv_autoStatus: AutoCompleteTextView = dialogView.findViewById(R.id.spinnerStatus)
+        tv_autoStatus.setAdapter(adapterStatus)
+        tv_autoStatus.setOnItemClickListener { _, _, position, _ ->
+            val selectedText = adapterStatus.getItem(position)
+//            Toast.makeText(this, selectedText, Toast.LENGTH_LONG).show()
+        }
+
+        // customer dropdown
+        val adapterCus = ArrayAdapter(this, R.layout.item_drop_down_filter, customerArr)
+        val tv_autoCus: AutoCompleteTextView = dialogView.findViewById(R.id.spinnerCustomer)
+        tv_autoCus.setAdapter(adapterCus)
+        tv_autoCus.setOnItemClickListener { _, _, position, _ ->
+            val selectedText = adapterCus.getItem(position)
+//            Toast.makeText(this, selectedText, Toast.LENGTH_LONG).show()
+        }
+
+        // date picker
+        val datePickerText: TextInputEditText = dialogView.findViewById(R.id.datePicker)
+        val myCalendar = Calendar.getInstance()
+        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "dd/MM/yyyy"
+            val sdf = SimpleDateFormat(myFormat, Locale.UK)
+            val formattedDate = sdf.format(myCalendar.time)
+//            Log.d("dateABC", formattedDate)
+            datePickerText.setText(formattedDate)
+        }
+
+        datePickerText.setOnClickListener {
+            DatePickerDialog(
+                this,
+                R.style.MyDatePickerDialogStyle, datePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
+        botDialog.show()
     }
 
 
